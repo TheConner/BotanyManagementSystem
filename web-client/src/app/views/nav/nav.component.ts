6be5ab6305dd7beca768765aceff8cbdfc114e3b
Conner Bradley <bradley@advtech.ca>
2020-12-28 20:03:00 -0500

    D A R K  M O D E

--- web-client/src/app/views/nav/nav.component.ts
+++ web-client/src/app/views/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { BMSService } from 'src/app/service/bms.service';
 
@@ -9,13 +9,19 @@ import { BMSService } from 'src/app/service/bms.service';
 })
 export class NavComponent implements OnInit {
   public user: any;
-  constructor(private api: BMSService) { }
+  public CurrentMode: string = '';
+  constructor(private api: BMSService, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.api.getUser()
     .subscribe((user) => {
       this.user = user;
     })
+  }
+
+  SetMode(mode) {
+    this.CurrentMode = mode;
+    this.renderer.setAttribute(document.documentElement, 'theme', mode);
   }
 
 }
